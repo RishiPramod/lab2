ef481a31046d21874f742b77953df9f4296a9363
Rishi Pramod <rishipramodc@gmail.com>
2026-01-09 23:52:02 +0530

    Experiment-2 Ridge Regression alpha=1.0 with scaling

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Paths
@@ -32,7 +32,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Model
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Evaluation
